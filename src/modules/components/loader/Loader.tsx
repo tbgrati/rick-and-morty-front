75a5150515ced1756682f9a9fb1fea3e108c211e
tbgrati <tobiassgrati@gmail.com>
2025-02-16 16:48:56 -0300

    Added Loader and NotFound page

---
+++ src/modules/components/loader/Loader.tsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+  return (
+    <div className="flex justify-center items-center max-w-150 max-h-150">
+      <div className="w-10 h-10 border-4 border-primary-400 border-t-ram-blue-600 rounded-full animate-spin"></div>
+    </div>
+  );
+};
--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,21 @@
-
+import { Link } from "react-router-dom";
 
 export const NotFoundPage = () => {
-    return (
-        <div>
-            Not found
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center items-center min-h-screen flex-col -translate-y-10">
+      <span className="relative text-white font-black block overflow-hidden text-[20.4em] w-fit h-max first-letter:tracking-[12vmax] before:content-[''] before:absolute before:h-full before:w-full before:bg-no-repeat before:bg-contain before:bg-center before:bg-[url('https://staticdelivery.nexusmods.com/mods/1151/images/528-0-1447526230.png')] animate-rotateIn">
+        44
+      </span>
+      <div className={"flex items-center flex-col gap-y-6"}>
+        <h2 className="text-gray-400 text-2xl">
+          We couldn't find the page you were looking for
+        </h2>
+        <Link to={`/`}>
+          <h1 className="hover:text-orange-500 font-semibold text-lg border-2 rounded-full px-4 py-2">
+            Take me home
+          </h1>
+        </Link>
+      </div>
+    </div>
+  );
+};
